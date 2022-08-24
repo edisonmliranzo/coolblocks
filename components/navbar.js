302ce5153be22fe2f148e6bf5d7fd44023cb2e0b
Edinson Liranzo <edison0220@gmail.com>
2022-08-23 21:18:00 -0400

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -4,11 +4,11 @@ import { Disclosure } from "@headlessui/react";
 
 export default function Navbar() {
   const navigation = [
-    "Product",
-    "Features",
-    "Pricing",
-    "Company",
-    "Blog",
+    "Home",
+    "NFTs MarketPlace",
+    "Metaverse",
+    "Play2Earn",
+    "Likesmeet",
   ];
 
   return (
